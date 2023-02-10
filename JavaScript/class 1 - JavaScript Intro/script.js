4fb9352ea2b1c8624f7beef9f47295fe7750c97d
haideralishah <headeralishah@gmail.com>
2023-02-10 18:02:30 +0500

    JS intro

---
+++ JavaScript/class 1 - JavaScript Intro/script.js
@@ -0,0 +1,151 @@
+// Learning new JS functions
+
+// alert("Welcome Haider!");
+
+// console.log("We are learning JS!");
+
+
+
+
+
+
+// alert(4 + 5);
+// console.log("5" + "2");
+// console.log('Haider' + " " + "Ali" + " " + 'Shah');
+
+
+
+
+
+
+
+// var student= "Imran";
+// var fee = 400;
+
+// console.log(student);
+
+// console.log(fee);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var firstName = "Haider";
+// var lastName = 'Ali';
+
+// console.log("Welcome " + firstName + " " + lastName);
+
+
+
+
+
+
+
+
+
+
+
+// var bill = 800;
+// var tax = 18;
+// var totalTax = bill / 100 * tax;
+// var totalBill = bill + totalTax;
+// console.log(totalBill);
+
+
+
+
+
+
+
+
+
+
+// var question = "what is your fathers name?"
+
+// prompt(question);
+
+
+
+
+
+
+
+// var userName = prompt('What is your name?');
+
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var defName = "Saylani Welfare Internation Trust";
+var question = 'Where do you study?'
+
+var instName = prompt(question, defName);
+
+console.log(instName);
+
+
+
+
+
