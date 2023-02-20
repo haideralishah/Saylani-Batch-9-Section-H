7df64bb106adee8bd10a0658ef997c6e93a281f0
haideralishah <headeralishah@gmail.com>
2023-02-20 08:56:15 -0500

    boolean ++ -- if else

---
+++ JavaScript/class 4 - 1 to 9 chapters/script.js
@@ -0,0 +1,247 @@
+
+
+// console.log(5 + 9); // 14
+
+// console.log(5 - 9); // -4
+
+// console.log(5 * 9); // 45
+
+// console.log(45 / 9); // 5
+
+// console.log(49 % 9); // 4
+
+
+
+// var equation = (5 + (8 - 7)) * 20 / 4; 
+// console.log(equation);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// + - * / % () ** 
+
+
+
+
+
+
+
+// var count = 10;
+
+// console.log(count++);  //  10
+
+// console.log(count++); // 11
+
+// console.log(count); //  12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var count = 17;
+
+// console.log(count++);  //  17
+
+// console.log(++count); // 19
+
+// console.log(count); //  19
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var count = 17;
+
+// console.log(--count);  //  16
+
+// console.log(count--); // 16
+
+// console.log(--count); // 14 
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 5;
+// var num2 = 7;
+
+// var total = ++num1 + num2--  - num1++ + ++num2 - ++num2
+// //           6     +  7    -  6     +  7      -  8
+
+// console.log(total);
+
+
+
+
+
+
+
+
+
+// var num1 = 1;
+// var num2 = 3;
+
+// var total = num2++ + ++num1 - ++num1 + ++num2;
+// //           3     +   2    -  3     +  5
+
+// console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+// var evenCount = 2;
+// console.log(evenCount); // 2
+
+// evenCount = evenCount + 2;
+// console.log(evenCount);  // 4
+
+// evenCount = evenCount + 5;
+// console.log(evenCount); //  9
+
+
+
+
+
+
+
+
+
+
+// var userName = "Haider";
+
+// userName = userName + " Ali";
+
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 7;
+// num1 = num1 - 3;
+
+// console.log(num1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/class 5 - Boolean, if else/script.js
@@ -0,0 +1,96 @@
+
+// boolean
+// true false;
+
+// var num1 = 2 + 2;
+
+
+// console.log(num1 === 6);
+// console.log(num1 !== 4); 
+// console.log(num1 > 4);
+// console.log(num1 < 7);
+// console.log(num1 >= 4);
+// console.log(num1 <= 7);
+
+
+
+
+// var registeredUser = "haider"; 
+
+// var userInput = prompt('what is your name?');
+
+// console.log(registeredUser === userInput);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var registeredUser = "haider"; 
+
+// var userInput = prompt('what is your name?');
+
+// if(registeredUser === userInput){
+//     console.log('welcome sir ' + registeredUser);
+// }
+// else{
+//     console.log('please register first.');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var reqMarks = 90;
+var result = +prompt("Please enter your marks");
+
+if(result >= reqMarks){
+    console.log('I will buy you a heavy bike');
+}
+else{
+    console.log('please bring my chittar');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
