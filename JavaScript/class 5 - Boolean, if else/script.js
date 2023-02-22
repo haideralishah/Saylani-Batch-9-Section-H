c48d2ee7bca7dec216cf18877bee8805a952923c
haideralishah <headeralishah@gmail.com>
2023-02-22 08:42:50 -0500

    if else condition learned

--- JavaScript/class 5 - Boolean, if else/script.js
+++ JavaScript/class 5 - Boolean, if else/script.js
@@ -1,27 +1,25 @@
 
-// boolean
-// true false;
+// // boolean
+// // true false;
 
-// var num1 = 2 + 2;
-
-
-// console.log(num1 === 6);
-// console.log(num1 !== 4); 
-// console.log(num1 > 4);
-// console.log(num1 < 7);
-// console.log(num1 >= 4);
-// console.log(num1 <= 7);
+// // var num1 = 2 + 2;
 
 
+// // console.log(num1 === 6);
+// // console.log(num1 !== 4); 
+// // console.log(num1 > 4);
+// // console.log(num1 < 7);
+// // console.log(num1 >= 4);
+// // console.log(num1 <= 7);
 
 
-// var registeredUser = "haider"; 
-
-// var userInput = prompt('what is your name?');
-
-// console.log(registeredUser === userInput);
 
 
+// // var registeredUser = "haider"; 
+
+// // var userInput = prompt('what is your name?');
+
+// // console.log(registeredUser === userInput);
 
 
 
@@ -36,15 +34,43 @@
 
 
 
-// var registeredUser = "haider"; 
 
-// var userInput = prompt('what is your name?');
 
-// if(registeredUser === userInput){
-//     console.log('welcome sir ' + registeredUser);
+// // var registeredUser = "haider"; 
+
+// // var userInput = prompt('what is your name?');
+
+// // if(registeredUser === userInput){
+// //     console.log('welcome sir ' + registeredUser);
+// // }
+// // else{
+// //     console.log('please register first.');
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var reqMarks = 90;
+// var result = +prompt("Please enter your marks");
+
+// if(result >= reqMarks){
+//     console.log('I will buy you a heavy bike');
 // }
 // else{
-//     console.log('please register first.');
+//     console.log('please bring my chittar');
 // }
 
 
@@ -63,15 +89,198 @@
 
 
 
-var reqMarks = 90;
-var result = +prompt("Please enter your marks");
 
-if(result >= reqMarks){
-    console.log('I will buy you a heavy bike');
-}
-else{
-    console.log('please bring my chittar');
-}
+
+
+
+
+
+// var tableName = +prompt('Enter table name');//2
+// var count = 1;
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+// document.write(tableName + " X " + count + " = " + count++ * tableName + "<br />");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var age = +prompt('what is your age?');
+// var p = document.querySelector('.msg');
+// var sorryMsg ='I am sorry, you are not eligible.'
+// var congratsMsg = "Congrats! You have got the admission."
+// if(age >= 15){
+//    p.innerHTML = sorryMsg;
+// }
+// else{
+//     p.innerHTML = congratsMsg;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var marks = prompt("enter your marks");
+// var msgPara = document.querySelector('.msg');
+// if(marks >= 80){
+//     msgPara.innerHTML = "Congrats! you have got A+"
+// }
+// else if(marks >=70){
+//     msgPara.innerHTML = 'You have got A';
+// }
+// else if(marks >= 60){
+//     msgPara.innerHTML = 'You have got B';
+// }
+// else if(marks >= 50){
+//     msgPara.innerHTML = 'You have got C';
+// }
+// else if(marks >= 40){
+//     msgPara.innerHTML = 'You have got D';
+// }
+// else if(marks >= 33){
+//     msgPara.innerHTML = 'You have got E';
+// }
+// else{
+//     msgPara.innerHTML = 'You are failed.';
+// }
+
+
+
+
+
+// var age = prompt('enter your age');
+// var lastScore = prompt('what was your last score'); 
+
+// var msgPara = document.querySelector('.msg');
+// if(age < 15){
+//     msgPara.innerHTML = "Congrats! you have got the admission in sports club"
+// }
+// if(lastScore >= 75){
+//     msgPara.innerHTML = msgPara.innerHTML + "<br /> Congrats! you have got the admission in University"
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var msgPara = document.querySelector('.msg');
+// var age = prompt('enter your age');
+// var gender = prompt("enter your gender");
+// if(age < 15 && gender === "male"){
+//     msgPara.innerHTML = "you have got the admission.";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var msgPara = document.querySelector('.msg');
+// var lastScore = +prompt('what is your last score?');
+// var isDisabled = confirm('are you disabled');
+
+// if(lastScore >75 || isDisabled){
+//     msgPara.innerHTML = "you have got the admission.";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
