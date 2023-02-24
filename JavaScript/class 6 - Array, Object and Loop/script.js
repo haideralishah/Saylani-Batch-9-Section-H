33a37da206e6eb8f7847cbaac9e956c107877515
haideralishah <headeralishah@gmail.com>
2023-02-24 09:43:24 -0500

    array learnt

---
+++ JavaScript/class 6 - Array, Object and Loop/script.js
@@ -0,0 +1,309 @@
+
+
+// var isMatricPassed = confirm("have you passed matric?");
+// var testScore = +prompt('what was your test score?');
+// var p = document.querySelector(".msg");
+// p.style.color = "#fff";
+// p.style.padding = "20px";
+// if(isMatricPassed && testScore >= 70){
+//     p.innerHTML = "Congrats! You have got the admission.";
+//     p.style.backgroundColor = "Green";
+// }
+// else{
+//     p.innerHTML = "Please try again later.";
+//     p.style.backgroundColor = "red";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var isSaylaniStudent = confirm("Are you already enrolled in Saylani");
+// var doYouKnowCoding = confirm('Do you know coding?');
+// var pElement = document.querySelector(".msg");
+// pElement.style.color = 'white';
+// pElement.style.padding = "20px";
+// if(isSaylaniStudent || doYouKnowCoding){
+//     pElement.innerHTML = "Congrats! You are allowed.";
+//     pElement.style.backgroundColor = "green";
+// }
+// else{
+//     pElement.innerHTML = "Sorry! you are not allowed.";
+//     pElement.style.backgroundColor = "red";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var p = document.querySelector(".msg");
+// var domecile = prompt('which domecile do you have?');
+
+// if(domecile === 'karachi'){
+//     var isEducated = confirm('are you educated?');
+//     if(isEducated){
+//         p.innerHTML = 'Congrats! you have secured this job.';
+//     }
+// }
+// else{
+//     p.innerHTML= "Sorry! you are not eligible.";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var students = ['Haider', "abdullah", 'Ahmed'];
+// console.log(students[0]);
+
+
+
+
+
+
+
+
+
+
+// var mixedArr = ["haider", 400, true];
+// console.log(mixedArr[2]);
+
+
+
+
+
+
+
+
+// var users = [];
+
+// users[0] = "haider";
+// users[1] = "abdullah";
+// users[5] = 'ahmed';
+// console.log(users);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var users = ["Ahmed"];
+// console.log(users);
+
+// users[0] = prompt('enter student name?');
+// users[1] = prompt('enter student name?');;
+// users[2] = prompt('enter student name?');;
+// console.log(users);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fruit = "apple";
+// var fruits = [fruit, 'banana', 'orange'];
+
+// console.log(fruits)
+
+
+
+
+
+// var cities = ['karachi', 'lahore', 'islamabad'];
+// // var msg = "Welcome to " + cities[2];
+// // console.log(msg);
+// cities[3] = 'peshawar';
+
+// console.log(cities.length);
+
+
+
+
+
+
+// var cities = ['karachi', 'lahore'];
+
+// cities.push("islamabad");
+
+// cities.push('peshawar');
+
+// cities.push('quetta');
+
+// console.log(cities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var cities = ['karachi', 'lahore'];
+
+// var userFavCity = prompt("enter your favorite city");
+
+// cities.push(userFavCity);
+
+// console.log(cities);
+
+
+
+
+
+
+
+
+
+
+// var cities = ['karachi', 'lahore'];
+// cities.length  //arrey length
+// cities.push("islamabad") //adds in last
+// cities.unshift('islamabad') // add in begining
+
+// cities.unshift("islamabad");
+// cities.push('islmabad', 'quetta', 'peshawar')
+// cities.unshift('islmabad', 'quetta', 'peshawar')
+
+
+// console.log(cities);
+
+
+
+
+
+
+
+
+
+
+// var cities = ['karachi', 'lahore', 'quetta'];
+// console.log(cities);
+
+// cities.pop(); //remove last index
+
+// cities.shift(); // remove first index
+// console.log(cities);
+
+
+
+
+
+// var cities = ['karachi','lahore', 'islamabad', 'peshawar', 'quetta'];
+
+// cities.splice(2, 0, "Gilgit");
+
+// console.log(cities);
+
+
+
+
+
+
+/*
+array.length;
+array.push()
+array.unshift()
+
+array.pop()
+array.shift()
+
+array.splice()
+
+*/
+
+
+
+
+
+
+
