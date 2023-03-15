7f91f4b587e2875776df17f54ab520b33bf1e1d7
haideralishah <headeralishah@gmail.com>
2023-03-15 11:27:32 -0400

    JS methods

--- JavaScript/class 8- String, Numbers, Boolean, Array, Objects and their Methods/script.js
+++ JavaScript/class 8- String, Numbers, Boolean, Array, Objects and their Methods/script.js
@@ -55,8 +55,23 @@ string.length;
 
 string.slice();
 
+string.replace();
+
+string.split();
+
 */
 
+
+
+/*
+Numbrer Methods
+=============
+Math.round();
+Math.ceil();
+Math.floor();
+Math.random();
+
+*/
 
 // var userName = 'The quick brown, fox jumps, over the, lazy dog.';
 
@@ -84,21 +99,240 @@ string.slice();
 
 
 
-var p = document.querySelector(".message");
+// var p = document.querySelector(".message");
 
-function checkPalindrome(){
-    var userInput = prompt('enter any word');
-    var userInputClone = userInput.slice(0);
-    userInputClone = userInputClone.split("")
-    userInputClone = userInputClone.reverse().join("");
+// function checkPalindrome(){
+//     var userInput = prompt('enter any word');
+//     var userInputClone = userInput.slice(0);
+//     userInputClone = userInputClone.split("")
+//     userInputClone = userInputClone.reverse().join("");
 
-    if(userInput === userInputClone){
-        p.innerHTML = userInput + " and " + userInputClone  + ' are palindrome'
-    }
-    else{
-        p.innerHTML = userInput + " and " + userInputClone  + ' are not palindrome'
-    }
-}
+//     if(userInput === userInputClone){
+//         p.innerHTML = userInput + " and " + userInputClone  + ' are palindrome'
+//     }
+//     else{
+//         p.innerHTML = userInput + " and " + userInputClone  + ' are not palindrome'
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var para = "Lorem ipsum dolor World War II sit amet consectetur World War II adipisicing World War II elit."
+// var replacedWord = "the second world war";
+// for(var i = 0; i < para.length; i++){ 
+//     var onCheckWord = para.slice(i, i + 12);
+
+//     if(onCheckWord === "World War II"){
+//         para = para.slice(0, i) + replacedWord + para.slice(i + 12);
+//        console.log(para);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// var text = "Lorem ipsum dolor World War II sit amet consectetur World War II adipisicing World War II elit."
+// var firstChar = text.indexOf("World War II");
+
+// if (firstChar !== -1) {
+//  text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+// }
+
+// console.log(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var para = "Lorem ipsum dolor World War II sit amet consectetur World War II adipisicing World War II elit."
+// var newWord = "the second world war";
+
+// para = para.replace(/World War II/g, newWord);
+
+// console.log(para);
+
+
+/*
+Home Excercise
+
+Note: Do not use split reverse join and replace methods
+
+Example:
+The-Quick-Brown
+customSplit(str, seperator)
+str.split("-")
+
+1. split a string
+2. reverse an array
+3. join an array
+4. replace any word from large string
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = 2.5849;
+// num = Math.round(num);
+// console.log(num);
+
+
+
+
+// var num = 100.2;
+// num = Math.ceil(num);
+// console.log(num);
+
+
+
+
+
+
+
+
+// var num = 100.9;
+// num = Math.floor(num);
+// console.log(num);
+
+
+
+
+
+
+
+// var num = 100.9;
+// num = Math.floor(num);
+// console.log(num);
+
+
+
+
+
+
+// var num = -10.9; 
+// num = Math.floor(num);
+// console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = -2.9; 
+// num = Math.ceil(num);
+// console.log(num);
+
+
+
+
+
+
+
+
+
+// var ranNum = Math.random();
+// console.log(ranNum);
+
+
+
+
+
+
+
+
+
+
+// var ranNum = Math.ceil(Math.random() * 2 );
+// console.log(ranNum);
+
+// if(ranNum === 1 ){
+//     console.log('it is head');
+// }
+// else{
+//     console.log('it is tail');
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
