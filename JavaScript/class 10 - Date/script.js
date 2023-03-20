8b30e4ce318ead2db778c6f4ad5c076e8e9f3e9e
haideralishah <headeralishah@gmail.com>
2023-03-20 17:55:34 +0500

    date introduced

---
+++ JavaScript/class 10 - Date/script.js
@@ -0,0 +1,76 @@
+
+// var d = new Date();
+// // console.log(d.toString());
+
+// var days = ['Sun', "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var dayNum = d.getDay();
+// console.log(days[dayNum]);
+
+// console.log(d.getDate());
+
+// var monthsName = ["Jan", "Feb", "Mar", "April"];
+// var monthNum = d.getMonth();
+// console.log(monthsName[monthNum]);
+
+
+// console.log(d.getFullYear());
+
+
+// console.log(d.getHours());
+
+// console.log(d.getMinutes());
+
+// console.log(d.getSeconds());
+
+// console.log(d.getMilliseconds());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var d = new Date("October 4, 1991");
+
+console.log(d.getFullYear());
+
+
