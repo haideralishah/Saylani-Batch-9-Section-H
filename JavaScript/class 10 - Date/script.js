00a73d3faee2532b01cc64cefb592bd8aca390ae
haideralishah <headeralishah@gmail.com>
2023-03-29 07:52:54 +0500

    functions covered

--- JavaScript/class 10 - Date/script.js
+++ JavaScript/class 10 - Date/script.js
@@ -69,8 +69,209 @@
 
 
 
-var d = new Date("October 4, 1991");
+// var d = new Date("October 4, 1991");
 
-console.log(d.getFullYear());
+// console.log(d.getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var d = new Date();
+// console.log(d, d.getTime());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var doomsday = new Date("June 30, 2024").getTime();
+// var today = new Date().getTime();
+// var diff = doomsday - today
+
+// console.log(Math.floor(diff / (1000 * 60 * 60 * 24 )));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var dob = new Date("October 4, 2023");
+// var today = new Date();
+
+// console.log(Math.floor((dob - today) / (1000 * 60 * 60 * 24)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userDOB = new Date("January 10, 1997");
+// var today = new Date();
+// console.log(Math.floor((today - userDOB) / (1000 * 60 *60 *24 * 365)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var now = new Date().setHours(30);
+// console.log(now)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var now = new Date().setHours(12);
+// now = new Date(now);
+// var h = now.getHours();
+
+// if(h > 12){
+//     var reqHour = h - 12;
+//     console.log(reqHour + "PM");
+// }
+// else if(h === 12){
+//     console.log(h + "PM");
+// }
+// else{
+//     console.log(h+ "AM");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ JavaScript/class 11 - Functions/script.js
@@ -0,0 +1,208 @@
+// var screen = document.querySelectorAll('#screen')[0];
+
+// var operator, num1="0", num2="0";
+
+// function num(numParameter) {
+//     if (operator) {
+//         // work for num2
+//         num2 =  num2 + numParameter;
+//         screen.innerHTML = Number(num2);
+//     }
+//     else {
+//         // work for num1
+//         num1 = num1 + numParameter;
+//         screen.innerHTML = Number(num1);
+//     }
+// }
+
+// function setOperator(operatorParameter) {
+//     operator = operatorParameter;
+//     screen.innerHTML = 0;
+// }
+
+// function calculate() {
+//     if (operator === "+") {
+//         screen.innerHTML = Number(num1) + Number(num2);
+//     }
+//     else if (operator === "-") {
+//         screen.innerHTML = Number(num1) - Number(num2);
+//     }
+//     else if (operator === "x") {
+//         screen.innerHTML = Number(num1) * Number(num2);
+//     }
+//     else if (operator === "÷") {
+//         screen.innerHTML = Number(num1) / Number(num2);
+//     }
+// }
+
+// function clearAll(){
+//     operator = undefined;
+//     num1="0";
+//     num2="0";
+//     screen.innerHTML = "0";
+// }
+
+
+
+// calculator code end here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = "haider";
+
+// function greetUser(){
+//     var userName = "Ali";
+//     console.log("Welcome " + userName);
+// }
+
+// console.log(userName);
+// greetUser();
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+
+
+// var sum;
+
+// function sum2Num(num1, num2) {    
+//     sum = num1 + num2;
+// }
+// sum2Num(2, 4);
+
+// function printSum() {
+//     console.log(sum);
+// }
+// printSum();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = prompt('what is your name?'); // haider
+
+
+
+// function sum(num1, num2) {
+//     alert('Hello');
+//     var total = num1 + num2;
+//     return total;    
+// }
+
+// var totalOfTwo = sum(4, 6);
+
+// console.log(totalOfTwo);
+
+
+
+
+
+
+function generateFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+var fullName = generateFullName("Haider", "Ali");
+
+console.log(fullName);
+
+
+var fullName = generateFullName("Akram", "Khan");
+
+console.log(fullName);
+
+
+
+
+
+
+
+
+
+
+
+
+
