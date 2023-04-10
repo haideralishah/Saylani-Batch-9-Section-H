b891937e10f89cea887d11fbc4740255262e4d83
haideralishah <headeralishah@gmail.com>
2023-04-10 10:33:29 -0400

    events covered

---
+++ JavaScript/class 14 - Events/script.js
@@ -0,0 +1,150 @@
+// My JavaScript
+
+/*
+=================================
+----------My JavaScript----------
+=================================
+*/
+
+
+
+
+
+
+
+
+// function greetFromDiv(){
+//     console.log('hello inside div');
+// }
+
+
+
+// var btn = document.querySelectorAll('button')[0];
+// function greetUser(){
+//     console.log('Hello World');
+// }
+// btn.addEventListener("click", greetUser);
+
+// btn.removeEventListener("click", greetUser);
+
+// btn.addEventListener("click", function(){
+//     console.log('call back function')
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function customDestination(){
+//     var userDestination = prompt('where you want to go?');
+//     location = userDestination;
+// }
+
+
+// function greetUser(msg){
+//     console.log(msg);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// var img = document.querySelectorAll('img')[0];
+// function swapImage(imageSource){
+//     img.src = imageSource;
+// }
+
+
+
+
+// var user = {
+//     userName: 'haider',
+//     rollNumber: 2777,
+//     greetMsg:  function (){
+//         console.log(this);
+//     }  
+// }
+
+// user.greetMsg();
+
+// function greetUser(){
+//     console.log(this);
+// }
+
+// var student = "Abdullah";
+
+// greetUser();
+// console.log(this);
+
+
+
+
+
+
+
+
+
+// function swapImage(element, newSource){
+//     element.src = newSource;
+// }
+
+
+
+
+
+
+
+
+
+// function changeColor(pElement){
+//     var colors = ['red', 'green', 'blue'];
+//     var ranColorIndx = Math.floor(Math.random() * 3);
+//     pElement.style.color = colors[ranColorIndx];
+
+// }
+
+
+
+
+
+
+
+// function checkEmail(){
+//     alert('check email')
+// }
+
+
+
+
+function makeInvisible(element){
+    // element.className = element.className + " hidden";
+    element.className += " hidden";
+
+}
+
+
