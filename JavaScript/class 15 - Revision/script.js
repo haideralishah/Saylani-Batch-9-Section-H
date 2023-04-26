7f310ce0ea2200e725e01035d405978956318cbe
haideralishah <headeralishah@gmail.com>
2023-04-26 17:51:54 +0500

    class code till DOM and Node Tree

---
+++ JavaScript/class 15 - Revision/script.js
@@ -0,0 +1,193 @@
+// var h1Element = document.querySelectorAll("h1")[0];
+// // document.querySelector();
+
+// console.log(h1Element);
+
+// var inputEl = document.querySelectorAll("#username")[0];
+// inputEl.value = 'Alex242';
+// console.log(inputEl.value);
+
+
+// var pElement = document.querySelectorAll("p")[0];
+// pElement.innerHTML = "Eid Mubarak";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if( (8 % 2 === 0 && 2 + 2 === 4) || visaStatus === "US citizen" ){
+
+// }
+// else if(){
+
+// }
+// else{
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+// var a = 5;
+// var b = 7;
+// var c = a;
+// if(a >= 5){ // true
+//     if(b <= a){
+//         console.log(3 )
+//     }
+//     else if(c >= a) {
+//         console.log(2)
+//     }
+// }
+// else if(c > a){
+//     console.log(1)
+// }
+
+
+
+
+
+
+
+// var totalSlots = [0, ... 10];
+// var bookedSlots = [{
+//     bookedSlot: 3,
+//     bookedDateandTime: 264791341897941 , // 7 to 8.30
+//     bookedDateandTimeEnd: 264791341897941 
+// },
+// {
+//     bookedSlot: 3,
+//     bookedDateandTime: 264791341897941 , // 8.30 to 9
+//     bookedDateandTimeEnd: 264791341897941 
+// },
+// {
+//     bookedSlot: 3,
+//     bookedDateandTime: 264791341897941 , // 2 to 4
+//     bookedDateandTimeEnd: 264791341897941 
+// }
+// ];
+
+// var requiredSlot = 3;
+// var requiredStartDate = 264791341897941; // 8.15 to 9
+// var requiredEndDate = 264791341897941;
+
+
+
+
+
+
+// function greetUser() {
+//     var userName = prompt('what is your name?');
+//     console.log("Welcome " + userName);
+// }
+
+
+// for(var i = 0; i < 2; i++){
+//     greetUser()
+// }
+
+
+
+
+
+
+
+
+
+// var totalRanNumbers = [];
+// while (new Date().getSeconds() % 2 === 0) {
+//     var ranNum = Math.ceil(Math.random() * 100);
+//     totalRanNumbers.push(ranNum);    
+// }
+
+// console.log(totalRanNumbers);
+
+
+
+
+
+// var spaces = [" ", " ", " ", " "," ", " ", " ", " "];
+// var stars = ["*", "*", "*", "*","*", "*", "*", "*"];
+// for(var i = 0 ; i < 8; i++){
+//     console.log(spaces.join("").slice(i) + stars.join("").slice(stars.length - i) + stars.join("").slice(stars.length - i) + spaces.join("").slice(i));
+// }
+
+
+
+
+
+
+
+
+
+
+// var h1Element = document.querySelectorAll("h1")[0];
+// h1Element.className = h1Element.className  + " bg-apply";
+// h1Element.className += " bg-apply";
+
+// h1Element.style.color = "red"
+// h1Element.style.backgroundColor = "yellow"
+// h1Element.style.padding = "20px 20px 5px 5px"
+// h1Element.style.marginTop = "50px"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/class 16 - DOM Tree and Nodes/script.js
@@ -0,0 +1,8 @@
+
+
+
+
+var h1Element = document.childNodes[1].childNodes[2].childNodes[1]
+var h1WithQuery = document.querySelectorAll('h1')[0];
+
+h1Element.innerHTML = 'Allah Hafiz';
